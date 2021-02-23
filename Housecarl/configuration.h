ce0208f8e9d80a975521f5553f400a5e43ecbd0e
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-02-23 19:05:51 +0100

    create configuration.h file

---
+++ Housecarl/configuration.h
@@ -0,0 +1,11 @@
+// Credentials
+// WiFi network credentials
+const char* ssid = "*****";
+const char* password = "*****";
+
+// Network config
+IPAddress ip(192, 168, 1, 250);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(208, 67, 222, 222);    // OpenDNS
+IPAddress secondaryDNS(8, 8, 8, 8);         // Google
